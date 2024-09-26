a331ceeb184ca6aa4ab7ed82360d2aeda01e4850
THOMAS <thomaslunardo@gmail.com>
2024-09-26 16:09:45 +0200

    quest done with style

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Article {
+  title: string;
+  author: string;
+  content: string;
+  image: string;
+  isPublished: boolean,
+  comment: string
+}
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
 
 export class ArticleComponent {
-  title = 'What is Lorem Ipsum?';
-  paragraph = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+
+  article1: Article = {
+    title: 'J\'voulais juste un mac moring',
+    author: 'Jean body',
+    content: 'Jules Winnfield, incarné par Samuel L. Jackson dans Pulp Fiction, est un tueur à gages charismatique et philosophe, célèbre pour son monologue biblique avant d\'exécuter ses victimes. Avec sa coupe afro et son regard perçant, il impose une présence marquante dans chaque scène. Son duo avec Vincent Vega, joué par John Travolta, mélange humour noir et tension. Mais au-delà de son rôle violent, Jules traverse une profonde transformation spirituelle. À la fin du film, il renonce à la violence après une « intervention divine », symbolisant son désir de rédemption et de changement.',
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTm4WgsTa2pNA6Z8oNr1nL1NlEQUPuKNX1TB40Q-kppjjQcYNKHphwKHbQiA&s",
+    isPublished: true,
+    comment: ''
+  }
+
+  sayMyName(): void {
+    alert("Say my name");
+  }
+
+  togglePublication(): void {
+    this.article1.isPublished = !this.article1.isPublished;
+  }
 }
