b2fc55fde0a8cc381a4e183d3368f9e90aa68ad0
THOMAS <thomaslunardo@gmail.com>
2024-09-27 09:42:00 +0200

    je sais plus

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -21,7 +21,7 @@ interface Article {
 export class ArticleComponent {
 
   article1: Article = {
-    title: 'J\'voulais juste un mac moring',
+    title: 'J\'voulais juste un mac morning',
     author: 'Jean body',
     content: 'Jules Winnfield, incarné par Samuel L. Jackson dans Pulp Fiction, est un tueur à gages charismatique et philosophe, célèbre pour son monologue biblique avant d\'exécuter ses victimes. Avec sa coupe afro et son regard perçant, il impose une présence marquante dans chaque scène. Son duo avec Vincent Vega, joué par John Travolta, mélange humour noir et tension. Mais au-delà de son rôle violent, Jules traverse une profonde transformation spirituelle. À la fin du film, il renonce à la violence après une « intervention divine », symbolisant son désir de rédemption et de changement.',
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTm4WgsTa2pNA6Z8oNr1nL1NlEQUPuKNX1TB40Q-kppjjQcYNKHphwKHbQiA&s",
