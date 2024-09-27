2dcf5b11acccfd3c9ae06cdc174678e8cbae05a0
THOMAS <thomaslunardo@gmail.com>
2024-09-27 17:46:18 +0200

    made loop and the condiction display on article and directives.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,83 @@
 import { Component } from '@angular/core';
 import { ArticleComponent } from './article/article.component';
+import { CommonModule } from '@angular/common';
+
+interface Article {
+  title: string;
+  author: string;
+  content: string;
+  image: string;
+  isPublished: boolean,
+  comment: string,
+  likes: number
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ArticleComponent],
+  imports: [ArticleComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'Bienvenue sur le Wild Blog de Thomas !';
+  
+  title: string = 'Bienvenue sur le Wild Blog de Thomas !';
+  
+  articles: Article[] = [
+    { 
+      title: 'Angular 16: Les nouveautés', 
+      author: 'Alice', 
+      content: 'Les nouveautés d\'Angular 16 incluent...', 
+      image: 'https://pbs.twimg.com/profile_images/1721593504118493184/UzPnGWvZ_400x400.jpg',
+      isPublished: true, 
+      comment: '', 
+      likes: 120 
+    },
+    { 
+      title: 'Développer une API REST', 
+      author: 'Bob', 
+      content: 'Développer une API REST nécessite...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: false, 
+      comment: '', 
+      likes: 75 
+    },
+    { 
+      title: 'Pourquoi TypeScript est essentiel ?', 
+      author: 'Charlie', 
+      content: 'TypeScript apporte de la robustesse...', 
+      image: 'https://www.datocms-assets.com/48401/1628645197-learn-typescript.png',
+      isPublished: true, 
+      comment: '', 
+      likes: 200 
+    },
+    { 
+      title: 'Pourquoi TypeScript est essentiel ?', 
+      author: 'Charlie', 
+      content: 'TypeScript apporte de la robustesse...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true, 
+      comment: '', 
+      likes: 140 
+    },
+    { 
+      title: 'Pourquoi TypeScript est essentiel ?', 
+      author: 'Charlie', 
+      content: 'TypeScript apporte de la robustesse...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true, 
+      comment: '', 
+      likes: 80 
+    },
+    { 
+      title: 'Pourquoi TypeScript est essentiel ?', 
+      author: 'Charlie', 
+      content: 'TypeScript apporte de la robustesse...', 
+      image: 'https://via.placeholder.com/350x150',
+      isPublished: true, 
+      comment: '', 
+      likes: 200 
+    }
+  ];
 }
