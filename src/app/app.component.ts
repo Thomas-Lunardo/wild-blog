cdcce28e70d54e978f2ff3fac5c95d2dbd9beddb
THOMAS <thomaslunardo@gmail.com>
2024-09-30 16:44:27 +0200

    made the contact route and interface Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from "./home-page/home-page.component";
 import { ArticlePageComponent } from './article-page/article-page.component';
 import { RouterOutlet } from '@angular/router';
+import { ContactFormComponent } from "./contact-form/contact-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ArticlePageComponent, CommonModule, HomePageComponent, RouterOutlet],
+  imports: [CommonModule, HomePageComponent, RouterOutlet, ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+
+  onSubmit(): void {
+    console.log(('Formulaire envoyé !'));
+  }
+}
