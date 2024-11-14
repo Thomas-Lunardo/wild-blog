c69cf3d6c481fcf786edeeb8ace2e7e259761eae
THOMAS <thomaslunardo@gmail.com>
2024-11-14 15:28:27 +0100

    put methods inside apiService and use them in components.

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,82 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { Article } from '../../models/article.model';
-import { ArticleComponent } from "../../components/article/article.component";
+import { Component } from '@angular/core';
 import { ArticleListComponent } from '../../components/article-list/article-list.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, RouterLink, ArticleComponent, ArticleListComponent],
+  imports: [CommonModule, ArticleListComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 
 export class HomePageComponent {
-  
-  articles: Article[] = [
-    { 
-      id: 1,
-      title: 'Angular 16: Les nouveautés', 
-      author: 'Alice', 
-      content: 'Les nouveautés d\'Angular 16 incluent...', 
-      image: 'https://via.placeholder.com/150x150',
-      isPublished: true, 
-      comment: '', 
-      likes: 120
-    },
-    { 
-      id: 2,
-      title: 'Développer une API REST', 
-      author: 'Bob', 
-      content: 'Développer une API REST nécessite...', 
-      image: 'https://via.placeholder.com/150x150',
-      isPublished: false, 
-      comment: '', 
-      likes: 75 
-    },
-    { 
-      id: 3,
-      title: 'Pourquoi Symfony est essentiel ?', 
-      author: 'Charlie', 
-      content: 'Symfony apporte de la robustesse...', 
-      image: 'https://via.placeholder.com/150x150',
-      isPublished: true, 
-      comment: '', 
-      likes: 200 
-    },
-    { 
-      id: 4,
-      title: 'Pourquoi Toto est essentiel ?', 
-      author: 'Charlie', 
-      content: 'Toto apporte de la robustesse...', 
-      image: 'https://via.placeholder.com/150x150',
-      isPublished: true, 
-      comment: '', 
-      likes: 140 
-    },
-    { 
-      id: 5,
-      title: 'Pourquoi TypeScript est essentiel ?', 
-      author: 'Charlie', 
-      content: 'TypeScript apporte de la robustesse...', 
-      image: 'https://via.placeholder.com/150x150',
-      isPublished: true, 
-      comment: '', 
-      likes: 80 
-    },
-    { 
-      id: 6,
-      title: 'Pourquoi TypeScript est essentiel ?', 
-      author: 'Charlie', 
-      content: 'TypeScript apporte de la robustesse...', 
-      image: 'https://via.placeholder.com/150x150',
-      isPublished: true, 
-      comment: '', 
-      likes: 200 
-    }
-  ];
 
   messageFromArticle: string = '';
 
